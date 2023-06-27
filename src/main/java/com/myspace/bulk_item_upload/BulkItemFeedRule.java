be3073d0843bd0f4403ba2017d5281fbe1f265a4
admin <>
2023-06-27 06:40:34 +0000

    {/src/main/java/com/myspace/bulk_item_upload/BulkItemFeedRule.java}

--- src/main/java/com/myspace/bulk_item_upload/BulkItemFeedRule.java
+++ src/main/java/com/myspace/bulk_item_upload/BulkItemFeedRule.java
@@ -6,12 +6,44 @@ package com.myspace.bulk_item_upload;
 
 public class BulkItemFeedRule implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public BulkItemFeedRule() {
-    }
+	private java.lang.String reprocessFailed;
+	private int maxRetries;
+	private boolean sendEmailOnMaxRetries;
 
+	public BulkItemFeedRule() {
+	}
 
+	public java.lang.String getReprocessFailed() {
+		return this.reprocessFailed;
+	}
 
+	public void setReprocessFailed(java.lang.String reprocessFailed) {
+		this.reprocessFailed = reprocessFailed;
+	}
+
+	public int getMaxRetries() {
+		return this.maxRetries;
+	}
+
+	public void setMaxRetries(int maxRetries) {
+		this.maxRetries = maxRetries;
+	}
+
+	public boolean isSendEmailOnMaxRetries() {
+		return this.sendEmailOnMaxRetries;
+	}
+
+	public void setSendEmailOnMaxRetries(boolean sendEmailOnMaxRetries) {
+		this.sendEmailOnMaxRetries = sendEmailOnMaxRetries;
+	}
+
+	public BulkItemFeedRule(java.lang.String reprocessFailed, int maxRetries,
+			boolean sendEmailOnMaxRetries) {
+		this.reprocessFailed = reprocessFailed;
+		this.maxRetries = maxRetries;
+		this.sendEmailOnMaxRetries = sendEmailOnMaxRetries;
+	}
 
 }
